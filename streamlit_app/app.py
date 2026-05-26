f2447243cdf5f34fbb363ec369038ab984d51785
CodeBonker <codebonkerhead@gmail.com>
2026-05-26 16:06:16 +0530

    Integrated file reading and dataframe preview in Streamlit

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Chunks Maker",
@@ -21,3 +22,25 @@ if uploaded_file is not None:
     st.success(
         f"Uploaded: {uploaded_file.name}"
     )
+
+    file_extension = (
+        uploaded_file.name
+        .split(".")[-1]
+        .lower()
+    )
+
+    if file_extension == "csv":
+
+        df = pd.read_csv(uploaded_file)
+
+    else:
+
+        df = pd.read_excel(uploaded_file)
+
+    total_rows = len(df)
+
+    st.write(
+        f"Total Rows Found: {total_rows}"
+    )
+
+    st.dataframe(df.head())
